f92df0269208ceb4b5a43c91ecf3fca800991941
youhui1214 <405852890@qq.com>
2020-03-21 15:05:59 +0800

    feat: 注册登录完成

--- server/index.js
+++ server/index.js
@@ -1,9 +1,43 @@
 // const Koa = require('koa')
 import Koa from 'koa'
+
+import mongoose from 'mongoose'
+import bodyParser from 'koa-bodyparser'
+import session from 'koa-generic-session'
+import Redis from 'koa-redis'
+import json from 'koa-json'
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 
 const app = new Koa()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+app.keys = ['mt', 'keyskeys']
+app.proxy = true
+app.use(session({ key: 'mt', prefix: 'mt:uid', store: new Redis() }))
+app.use(bodyParser({
+  extendTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(dbConfig.dbs, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('mongoose Connection success!')
+  }
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -13,17 +47,13 @@ async function start () {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
 
-  const {
-    host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
-  } = nuxt.options.server
-
-  await nuxt.ready()
   // Build in development
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  app.use(users.routes()).use(users.allowedMethods())
 
   app.use((ctx) => {
     ctx.status = 200
